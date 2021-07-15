dba1ff87ed98d02f6c60480930a8d7f1c288afc5
Janik  Steegmüller <janik@steegmueller.dev>
2021-07-15 14:58:44 +0200

    feat: add multi-column layout

--- src/app/energy-systems/energy-systems.module.ts
+++ src/app/energy-systems/energy-systems.module.ts
@@ -6,6 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexModule } from '@angular/flex-layout';
+import {
+  MatFormFieldControl,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [EnergySystemsComponent, EnergySystemDetailComponent],
@@ -15,6 +22,10 @@ import { MatCardModule } from '@angular/material/card';
     MatButtonModule,
     MatListModule,
     MatCardModule,
+    MatGridListModule,
+    FlexModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
 export class EnergySystemsModule {}
